a8dc4ce375be2e09758a134316eb51e831c50dee
DiegoBecerra613 <diegoantoniobecerrapicon@gmail.com>
2024-04-15 21:01:31 -0600

    Funcion EditarLista

--- javascript/mitablero.js
+++ javascript/mitablero.js
@@ -51,10 +51,10 @@ onAuthStateChanged(auth, async (user) => {
 
     // Agregar evento click al botón para redirigir al usuario a la página crear-grupo.html
     document.querySelector('.btn_tomar_asistencia').addEventListener('click', function() {
-        window.location.href = 'EditarLista';
+        window.location.href = 'TomarAsistencias';
     });
 
-    document.querySelector('.btn__editar_lista').addEventListener('click', function() {
+    document.querySelector('.btn_editar_lista').addEventListener('click', function() {
         window.location.href = 'EditarLista';
     });
 });
